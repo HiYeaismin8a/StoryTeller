752965f641aa8e00daa9f518565ce28282b84211
Daniel López <dalopezqu@ittepic.edu.mx>
2022-11-12 12:06:39 -0700

    Diseño avance

---
+++ app/src/main/java/mx/edu/ittepic/storyteller/Inicio.kt
@@ -0,0 +1,26 @@
+package mx.edu.ittepic.storyteller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_inicio.*
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+
+        //Animaciion
+        val btt = AnimationUtils.loadAnimation(this, R.anim.btt)
+
+        login_btnIngresar.startAnimation(btt)
+        login_lblBienvenido.startAnimation(btt)
+        login_lblDemo.startAnimation(btt)
+        login_ui_titleBackground.startAnimation(btt)
+
+        login_btnIngresar.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
